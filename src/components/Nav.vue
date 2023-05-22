5687808e2a21aa23260ce36144d29449663c1f63
saberzalla1998 <saber.zalla@student.hu.nl>
2023-05-22 17:21:50 +0200

    #1 Started with nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,9 +3,12 @@
 </script>
 
 <template>
-  <nav>
-
-  </nav>
+  <b-nav pills>
+    <b-nav-item active>Active</b-nav-item>
+    <b-nav-item>Link</b-nav-item>
+    <b-nav-item>Another Link</b-nav-item>
+    <b-nav-item disabled>Disabled</b-nav-item>
+  </b-nav>
 </template>
 
 <style scoped lang="scss">
---
+++ src/components/Wrappers/Wrapper.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <nav-header/>
+        <main>
+            <slot name="content"></slot>
+        </main>
+        <nav-footer/>
+    </div>
+</template>
+<script>
+    import header from "../nav-footer/header.vue";
+    // import footer from "../nav-footer/footer.vue";
+
+    export default {
+        props: ['showfooter'],
+        components: {
+            "nav-header": header,
+            // "nav-footer": footer,
+        },
+    };
+
+</script>
