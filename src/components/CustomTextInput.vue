6c61dac2219df97cb058b373be3912be3fda92a1
NorthLake <martijn.noordermeer@gmail.com>
2023-05-26 13:22:04 +0200

    custom text input

---
+++ src/components/CustomTextInput.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: string,
+  name: string,
+}>();
+defineEmits(['update:modelValue']);
+</script>
+
+<template>
+  <label :for="name"><slot /></label>
+  <input
+    :name="name"
+    :value="modelValue"
+    @input="$emit('update:modelValue', modelValue)"
+    v-bind="$attrs"
+  />
+</template>
---
+++ src/views/InputTest.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import CustomTextInput from '@/components/CustomTextInput.vue';
+import CustomButton from '@/components/CustomButton.vue';
+
+const textInput = '';
+</script>
+
+<template>
+  <main>
+    <CustomTextInput name="test" v-model="textInput" placeholder="test">Test</CustomTextInput><br>
+  </main>
+</template>
+
+<style scoped lang="scss">
+
+</style>
