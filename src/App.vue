8b49df47defe77fbb4f93769864ec66b9be403f7
saberzalla1998 <saber.zalla@student.hu.nl>
2023-05-29 13:42:36 +0200

    #1 add navbar and footer, style is next

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
-<script setup lang="ts">
-import Nav from '@/components/Nav.vue';
-import Footer from '@/components/Footer.vue';
-
-</script>
-
 <template>
-  <Nav />
-  <router-view></router-view>
-  <Footer />
+  <Header></Header>
+   <div class="container">
+     <div class="row mt-5">
+       <div class="col-4"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
+       <div class="col-8"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
+     </div>
+   </div>
+   <Footer></Footer>
 </template>
+<script>
+//importing bootstrap 5 Modules
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+export default {
+ 
+components: {
+   Header,
+   Footer
+ }
+}
+</script>
