a4358d942c085dc5767b81cec9bb25df81509bf2
Martijn Noordermeer <martijn.noordermeer@gmail.com>
2023-06-02 15:53:33 +0200

    Styling in App instead of all other elements

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
-<template>
-  <Header></Header>
-   <router-view></router-view>
-   <Footer></Footer>
-</template>
 <script setup lang="ts">
-//importing bootstrap 5 Modules
-import Header from '@/components/Header.vue'
-import Footer from '@/components/Footer.vue'
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 </script>
+
+<template>
+  <Header />
+  <div id="main-content">
+    <router-view></router-view>
+  </div>
+  <Footer />
+</template>
