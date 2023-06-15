34f5cfb492c57c0da05a692e7bd2db47296cbfb1
vitocarreira <victorcarreira2002@gmail.com>
2023-06-15 15:59:21 +0200

    repository method findAlternativeByName implemented

--- src/views/Information.vue
+++ src/views/Information.vue
@@ -1,4 +1,16 @@
 <script setup lang="ts">
+import { findAlternativeByName } from '@/data/alternative-repository';
+import { useRouter } from 'vue-router';
+
+const alternative = findAlternativeByName(document.title.split(" ")[2]);
+
+if (alternative === undefined) {
+    useRouter().push("undefined");
+
+} else {
+    
+
+}
 
 </script>
 
