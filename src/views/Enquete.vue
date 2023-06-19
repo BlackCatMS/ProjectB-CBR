1289b95bc1d59b8f1b03ff8b3b4b2bce08967801
NorthLake <martijn.noordermeer@gmail.com>
2023-06-19 16:18:32 +0200

    Basic PoC for enquete

--- src/views/Enquete.vue
+++ src/views/Enquete.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
+import CustomButton from '@/components/CustomButton.vue';
+import CustomRadioInput from '@/components/CustomRadioInput.vue';
+import { ref } from 'vue';
 
+const questions = [
+  {question: "Car go?", answers: ['moo', 'woof', 'vroom', 'meow']},
+  {question: "Plane go?", answers: ['zeem', 'zaam', 'ziim', 'zoom']},
+  {question: "Frog go?", answers: ['ribbit', 'honk', 'oink', 'cock-a-doodle-doo']}
+];
+let givenAnswers = ref(new Array(questions.length).fill(0));
+let currentQuestion = ref(0);
+
+// setInterval(() => console.log(givenAnswers), 1000);
+// setInterval(() => currentQuestion.value = ++currentQuestion.value % 3, 1000);
 </script>
 
 <template>
   <main>
-
+    <div v-for="(question, index) in questions" :key="index" :hidden="index !== currentQuestion">
+      <h1>{{ question.question }}</h1>
+      <CustomRadioInput :name="question.question" :possible-values="question.answers.map((answer, i) => {return {label: answer, value: i+1}})" v-model="givenAnswers[index]" />
+      <CustomButton v-if="index !== 0" @click="currentQuestion--">Vorige</CustomButton>
+      <CustomButton v-if="index !== questions.length - 1" @click="currentQuestion++" :disabled="givenAnswers[index] === 0">Volgende</CustomButton>
+      <CustomButton v-else :disabled="givenAnswers[index] === 0">Bekijk resultaten</CustomButton>
+    </div>
   </main>
 </template>
 
