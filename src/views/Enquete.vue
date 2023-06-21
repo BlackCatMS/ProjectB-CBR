edca15070f73582d53827284b74853a6fbd4fcf1
NorthLake <martijn.noordermeer@gmail.com>
2023-06-21 04:08:28 +0200

    Working enquete

--- src/views/Enquete.vue
+++ src/views/Enquete.vue
@@ -1,24 +1,22 @@
 <script setup lang="ts">
 import CustomButton from '@/components/CustomButton.vue';
 import CustomRadioInput from '@/components/CustomRadioInput.vue';
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
-import questions from '@/data/enquete.json';
-let givenAnswers = ref(new Array(questions.length).fill(0));
+import { enqueteStore } from '@/enqueteStore';
+
+// let givenAnswers = reactive(enqueteStore.answers);
 let currentQuestion = ref(0);
-
-// setInterval(() => console.log(givenAnswers), 1000);
-// setInterval(() => currentQuestion.value = ++currentQuestion.value % 3, 1000);
 </script>
 
 <template>
   <main>
-    <div v-for="(question, index) in questions" :key="index" :hidden="index !== currentQuestion">
+    <div v-for="(question, index) in enqueteStore.questions" :key="index" :hidden="index !== currentQuestion">
       <h1>{{ question.question }}</h1>
-      <CustomRadioInput :name="question.question" :possible-values="question.answers.map((answer, i) => {return {label: answer, value: i+1}})" v-model="givenAnswers[index]" />
+      <CustomRadioInput :name="question.question" :possible-values="question.options.map((option, i) => ({label: option, value: i+1}))" v-model="enqueteStore.answers[index]" />
       <CustomButton v-if="index !== 0" @click="currentQuestion--">Vorige</CustomButton>
-      <CustomButton v-if="index !== questions.length - 1" @click="currentQuestion++" :disabled="givenAnswers[index] === 0">Volgende</CustomButton>
-      <CustomButton v-else :disabled="givenAnswers[index] === 0">Bekijk resultaten</CustomButton>
+      <CustomButton v-if="index !== enqueteStore.questions.length - 1" @click="currentQuestion++" :disabled="enqueteStore.answers[index] === 0">Volgende</CustomButton>
+      <CustomButton v-else :disabled="enqueteStore.answers[index] === 0" @click="$router.push('/results')">Bekijk resultaten</CustomButton>
     </div>
   </main>
 </template>
